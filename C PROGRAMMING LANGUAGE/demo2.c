ada535b6bdd97c7aaf7e3f53ff2830573664ee71
Rucha-Pathak <pathakrucha848@gmail.com>
2025-05-30 05:28:52 +0530

    Add files via upload

---
+++ C PROGRAMMING LANGUAGE/demo2.c
@@ -0,0 +1,9 @@
+void main(){
+	
+	int n1,n2,res;
+	n1=10;
+	n2=20;
+	res = n1+n2;
+	
+	printf("Addition of %d and %d is %d",n1,n2,res);
+}
---
+++ C PROGRAMMING LANGUAGE/demo4.c
@@ -0,0 +1,10 @@
+void main(){
+	
+	int a,b,res;
+	
+	a = 10;
+	b = 20;
+	res = a+b;
+	
+	printf("Addition oof %d and %d is ",a,b,res);
+}
