c5ba9257e0e88726cade22aa3012df9b4c4313f8
Rucha-Pathak <pathakrucha848@gmail.com>
2025-06-05 12:15:20 +0530

    Add files via upload

---
+++ TEST_FBS/TEST-1/TEST/Q3.c
@@ -0,0 +1,14 @@
+//Print last 2 digits of number
+
+void main(){
+	
+	int n, n1;
+	
+	printf("Enter number:");
+	scanf("%d",&n);
+	n1 = n%100;
+	printf("last two digits of %d are %d",n,n1);
+	//printf("%d",n1);
+	
+	
+}
