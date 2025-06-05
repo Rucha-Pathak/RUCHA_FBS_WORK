a787de9f9e898f0843c3916aa1a45d67dfef98fb
Rucha-Pathak <pathakrucha848@gmail.com>
2025-06-05 23:46:55 +0530

    Add files via upload

---
+++ C PROGRAMMING LANGUAGE/Assignments/Assignment-0/A0/Q3.c
@@ -0,0 +1,12 @@
+void main(){
+	
+	float celsius , fahrenhit;
+	celsius = 75.3;
+	fahrenhit = (celsius*(9/5))+32;
+	
+	printf("%f celsius in fahrenhit is %.3f",celsius,fahrenhit);
+	
+	
+	
+	
+}
---
+++ C PROGRAMMING LANGUAGE/Assignments/Assignment-0/A0/Q4.c
@@ -0,0 +1,16 @@
+void main(){
+	
+	int a,b,c;
+	a = 10;
+	b = 20;
+	printf("numbers before swapping are %d and %d",a,b);
+	
+	c = a;
+	a = b;
+	b = c;
+	
+	printf("\nnumbers after swapping are %d and %d",a,b);
+	
+	
+	
+}
