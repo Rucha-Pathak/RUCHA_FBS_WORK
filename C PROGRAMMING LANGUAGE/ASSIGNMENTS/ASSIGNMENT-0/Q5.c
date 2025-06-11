93ee51f4a03fa1eb3153a6c13b082175819aa3d7
Rucha-Pathak <pathakrucha848@gmail.com>
2025-06-11 18:32:36 +0530

    Add files via upload

---
+++ C PROGRAMMING LANGUAGE/ASSIGNMENTS/ASSIGNMENT-0/Q5.c
@@ -0,0 +1,12 @@
+void main(){
+	
+	int a , square, cube;
+	a = 10;
+	square = a*a;
+	cube = a*a*a;
+	
+	printf("square of %d is %d",a,square);
+	printf("\n cube of %d is %d",a,cube);
+	
+	
+}
---
+++ C PROGRAMMING LANGUAGE/ASSIGNMENTS/ASSIGNMENT-0/Q8.c
@@ -0,0 +1,11 @@
+void main(){
+	
+	int length,width,perimeter;
+	length = 40;
+	width = 20;
+	
+	perimeter = 2*(length +width);
+	printf("perimeter of %d length and %d width is %d",length,width,perimeter);
+	
+
+}
