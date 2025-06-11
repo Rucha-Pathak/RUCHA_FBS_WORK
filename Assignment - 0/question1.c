5ea9038a0433da157f958d728ee012fc20c8b756
Rucha-Pathak <pathakrucha848@gmail.com>
2025-06-11 18:43:55 +0530

    Delete Assignment - 0 directory

--- Assignment - 0/question1.c
+++
@@ -1,8 +0,0 @@
-void main(){
-	
-	int a,b;
-	a = 10;
-	b = 20;
-	
-	printf("Addition of %d and %d is %d",a,b,a+b);
-}
--- Assignment - 0/question3.c
+++
@@ -1,8 +0,0 @@
-void main(){
-	
-	float length,height,area;
-	length = 20.0;
-	height = 30.0;
-	area = 0.5*length*height;
-	printf("Area of triangle with length %f and height %f is %f",length,height,area);
-}
