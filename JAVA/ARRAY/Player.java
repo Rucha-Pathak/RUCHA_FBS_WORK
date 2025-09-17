b14fae63a8aee6c7570af0a16c6de1e5f513a215
Rucha Pathak <pathakrucha848@gmail.com>
2025-09-17 17:49:47 +0530

    Add files via upload

--- JAVA/ARRAY/Player.java
+++ JAVA/ARRAY/Player.java
@@ -12,13 +12,13 @@ class Player
 	void acceptInfo()
   	{
 	   System.out.println("enter the name: ");
-	   String Name = sc.nextLine();
-    	   System.out,println("enter the noofmatches: ");
-	    int noofmatches = sc.nextInt();
-   	   System.out,println("enter the runscored: ");
-	    int runsscored = sc.nextInt();
-	   System.out,println("enter the wickets taken: ");
-	    int Wicketstaken = sc.nextInt();
+	   Name = sc.nextLine();
+    	   System.out.println("enter the noofmatches: ");
+	   noofmatches = sc.nextInt();
+   	   System.out.println("enter the runscored: ");
+	   runscored = sc.nextInt();
+	   System.out.println("enter the wickets taken: ");
+	   Wicketstaken = sc.nextInt();
  	    sc.nextLine();
          }
 
@@ -38,14 +38,38 @@ class Test
   public static void main(String[] args)
    {
  	Scanner sc = new Scanner(System.in);
-        Player[] players = new Player[10];
+        Player[] players = new Player[3];
 
 	for(int i=0;i<players.length;i++)
 	{
 	    System.out.println("enter details of player"+(i+1));
-	    
-
+	    players[i] = new Player();
+            players[i].acceptInfo();    
+	}
 	
+
+	System.out.println("\n=== Player Details ===");
+        for (int i = 0; i < players.length; i++) {
+             players[i].diplayInfo();
+        }
+
+	int maxrunscored = players[0]. runscored;
+	for (int i = 0; i < players.length; i++)
+	{
+             if(players[i].runscored>maxrunscored)
+			maxrunscored = players[i].runscored;
+        }
+	System.out.println("Maximum run scored : "+maxrunscored);
+
+	int maxWicketstaken = players[0]. Wicketstaken;
+	for (int i = 0; i < players.length; i++)
+	{
+             if(players[i].Wicketstaken>maxWicketstaken)
+			maxWicketstaken = players[i].Wicketstaken;
+        }
+	System.out.println("Maximum Wickets taken : "+maxWicketstaken);
+
+
 
    }//main method ends here 
 
