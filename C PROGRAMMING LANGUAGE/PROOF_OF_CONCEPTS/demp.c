cc2cd52895a28cdb9c5a763d4c82faf0afda52f5
Rucha Pathak <pathakrucha848@gmail.com>
2025-10-05 01:11:08 +0530

    Add files via upload

---
+++ C PROGRAMMING LANGUAGE/PROOF_OF_CONCEPTS/demp.c
@@ -0,0 +1,19 @@
+void main()
+{
+	int a=10;
+	int b=20;
+	int c = a+b;
+	printf("%d",c);
+	printf("\n");
+	printf("hello world");
+    printf("\n");
+	int x = printf("hello") +printf("world");
+	printf("%d",x);
+	printf("\n");
+	int y = printf("hello")>printf("fbs");
+	printf("%d",y);
+	printf("\n");
+	int z = printf("hello")+printf("fbs")*printf("abc")<printf("a");
+	printf("%d",z);
+	printf("\n");
+}
